01d90ccfc10a70b8c27acc585f644918da4b58dd
yazhini_gayathri <yazhinigayathri.2002@gmail.com>
2024-10-12 19:10:02 +0530

    connected with mongodb

--- app.js
+++ app.js
@@ -1,2 +1,42 @@
 const express = require("express");
+const Fetch = require("./connection");
+require("./connection");
+
+const app = express();
+
+app.use(express.json());
+app.set(express.urlencoded({extended:false}));
+
+const port = process.env.PORT || 3000;
+
+app.get("/",(req,res) => {
+    res.sendFile(__dirname+"/index.html");
+})
+
+app.post("/bill",async (req,res) =>{
+    try {
+        
+        const customer_name = req.body.customer_name;
+        const customer_number = req.body.customer_number;
+        console.log(customer_name,customer_number);
+
+        const user = await Fetch.create({customer_name,customer_number});
+        if(user){
+            res.status(200).redirect('/');
+        }else{
+            res.status(400);
+        }
+
+    } catch (err) {
+        console.log(err);
+    }
+}) 
+
+
+app.listen(port, () => {
+    console.log(`server is running on ${port} `);
+    
+});
+
+
 
---
+++ connection.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+
+const connection = async () => {
+    try {
+      await mongoose.connect('mongodb+srv://yazhini:yazhini@cluster0.xrihspk.mongodb.net/noble_evergreen');
+      console.log('Connected to MongoDB');
+    } catch (e) {
+      console.log('Connection error:', e);
+    }
+  };
+
+  connection();
+
+
+const customer_id_schema = new mongoose.Schema({
+    customer_name:{
+        type: String, 
+        required: true
+    },
+
+    customer_number:{
+        type: Number,
+        required: true
+    }
+})
+
+
+const Fetch = mongoose.model("Fetch", customer_id_schema);
+
+module.exports = Fetch;
+  
